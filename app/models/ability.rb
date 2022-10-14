3966c425cceaa52b9d696f6b22a252f2c31141da
Dylan <dylan1120long@gmail.com>
2022-10-14 19:16:25 -0400

    configured rolify, maybe working

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,16 @@ class Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
+      if user.has_role? :admin?
+        can :manage, :all
+      else
+        can :read, :all
+      end
+       
+    
+    
+    
+    # return unless user.present?
     #   can :read, :all
     #   return unless user.admin?
     #   can :manage, :all
